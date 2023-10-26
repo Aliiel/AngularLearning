93c1e60d46f02ecb7356f9bd488852d0ac3fcb66
Aliiel <leila.mouaci@gmail.com>
2023-10-26 16:50:46 +0200

    services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListPokemonComponent } from './pokemon/list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './pokemon/detail-pokemon/detail-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
